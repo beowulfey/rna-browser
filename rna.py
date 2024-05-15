fea437cc3e6bf179bd88ad8ab58aa34623f2c960
Aaron Wolfe <wolfe.aarond@gmail.com>
2024-05-15 11:55:17 -0400

    updated CSVs

--- rna.py
+++ rna.py
@@ -1,11 +1,11 @@
 import marimo
 
-__generated_with = "0.4.0"
+__generated_with = "0.5.2"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def __(level, mo):
+def __(level, mo, source):
     mo.vstack(
         [
             mo.md(
@@ -15,6 +15,7 @@ def __(level, mo):
         Data adapted from https://cengen.shinyapps.io/CengenApp/ and has data for all neurons (threshold 2).   
         """
             ),
+            mo.hstack(["Data source: ", source], justify="start"),
             mo.hstack(["Pathway resolution: ", level], justify="start"),
         ]
     )
@@ -25,7 +26,11 @@ def __(level, mo):
 def __(mo, pathway, pathway_genes_converted):
     ## Some UI stuff
 
-    data_tab = "PLACEHOLDER"
+    data_tab = mo.vstack(
+        [
+            pathway,
+        ]
+    )
 
     info_tab = mo.vstack(
         [
@@ -50,30 +55,12 @@ def __(mo, pathway, pathway_genes_converted):
 
 
 @app.cell
-def __():
-    ## scRNA ANALYSIS 1
-    #  This uses Meld to analyze the different pathways.
-
-    # Lets try going straight from the raw data
-    sample_info = [('GSM4040097', 'Pan-neural', '1'), ('GSM4040098', 'Pan-neural', '2'), ('GSM4040099', 'GABAergic neurons', '1'),
-               ('GSM404100', 'nmr-1 expressing neurons', '1'), ('GSM404101', 'Glutamatergic neurons', '1'), ('GSM404102', 'GABAergic neurons', '2')]
-
-
-    return sample_info,
-
-
-@app.cell
-def __():
-    return
-
-
-@app.cell
-def __(mo, reads):
+def __(mo):
     ## UI elements
-
+    source = mo.ui.dropdown(["Classic CeNGEN", "Bulk CeNGEN"], value="Bulk CeNGEN")
     level = mo.ui.dropdown(["LEVEL 1", "LEVEL 2", "LEVEL 3", "LEVEL 4"], value="LEVEL 1")
-    neuron = mo.ui.dropdown(reads.columns[1:], value="AIY")
-    return level, neuron
+    # neuron = mo.ui.dropdown(reads.columns[1:], value="AIY")
+    return level, source
 
 
 @app.cell
@@ -94,21 +81,23 @@ def __(category_genes, genes, level, mo, pathway):
     ## UI Elements: third tier dependence
 
     pathway_genes_converted = genes.loc[genes.index.intersection(category_genes[level.value][pathway.value].dropna())]
-    pathway_genes_converted.rename(pathway.value, inplace=True)
+    pathway_genes_converted.rename("Gene", inplace=True)
 
     mo.output.clear()
     return pathway_genes_converted,
 
 
-@app.cell(disabled=True)
-def __(pd):
-    ## DATA IMPORT V1
+@app.cell
+def __(pd, source):
+    ## DATA IMPORT
     #  Imports the categories (lists of genes in each category) at four levels of detail. Imports the CenGen Reads and pulls the gene names
     #  out into a separate dateframe.
 
-
     ## CENGEN DATASET
-    url_sc = "https://raw.githubusercontent.com/beowulfey/rna-browser/main/data/Liska_Single_Cell_TPM_threshold2.csv"
+    if source.value == "Bulk CeNGEN":
+        url_sc = "https://raw.githubusercontent.com/beowulfey/rna-browser/main/data/Bulk-CenGen_111521.csv"
+    else:
+        url_sc = "https://raw.githubusercontent.com/beowulfey/rna-browser/main/data/Liska_Single_Cell_TPM_threshold2.csv"
 
     ## WALHOUT LAB ANNOTATIONS
     url_cats = "https://raw.githubusercontent.com/beowulfey/rna-browser/main/data/four_level_categories.csv"
@@ -132,11 +121,13 @@ def __(pd):
 
     reads = pd.read_csv(url_sc)
     reads.set_index("Wormbase_ID", inplace=True)
-    for _col in reads.columns[4:]:
+    for _col in reads.columns[:]:
         reads[_col] = pd.to_numeric(reads[_col], errors="coerce")
 
     genes = reads["Gene_Name"]
     reads.drop(["Sequence_Name", "Gene_Name"], axis=1, inplace=True)
+
+    cells = reads.iloc[0].index.tolist()
     return (
         L1_genes,
         L2_genes,
@@ -144,6 +135,7 @@ def __(pd):
         L4_genes,
         category_genes,
         cats,
+        cells,
         genes,
         reads,
         url_1,
@@ -156,19 +148,55 @@ def __(pd):
 
 
 @app.cell
+def __(np, pathway_genes_converted, reads):
+    per_cell = {}
+    cell_sum = {}
+
+    for gene in pathway_genes_converted.index:
+        med = np.median([i for i in reads.loc[gene].to_list() if i > 0])
+
+        for cell in reads.loc[gene].index:
+            # if cell == 'ASER':
+            # print(gene, pathway_genes_converted.loc[gene], cell, reads.loc[gene].loc[cell],reads.loc[gene].loc[cell]/med)
+            if not cell in per_cell.keys():
+                per_cell[cell] = {}
+            if med > 0:
+                per_cell[cell][gene] = (reads.loc[gene].loc[cell]) / med
+            else:
+                per_cell[cell][gene] = 0
+
+    for cell in per_cell:
+        cell_sum[cell] = np.median([i for i in list(per_cell[cell].values()) if i > 0])
+        # cell_sum[cell] = sum(list(per_cell[cell].values()))/len(list(per_cell[cell].values()))
+    return cell, cell_sum, gene, med, per_cell
+
+
+@app.cell
+def __(cell_sum, px):
+    _sorted = {k: v for k, v in sorted(cell_sum.items(), key=lambda item: item[1])}
+    _filtered = {k: v for k, v in _sorted.items() if v > 0}
+    neur_fig = px.scatter(x=_filtered.values(), y=_filtered.keys(), height=len(_filtered.values()) * 20)
+    neur_fig.update_xaxes(type="log")
+    neur_fig.update_yaxes(type="category")
+    neur_fig.update_layout(yaxis={"dtick": 1})
+    return neur_fig,
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
 def __():
     import marimo as mo
     import pandas as pd
     import numpy as np
-    import scprep
-    import meld
     import plotly.express as px
 
     # making sure plots & clusters are reproducible
     np.random.seed(42)
-
-
-    return meld, mo, np, pd, px, scprep
+    return mo, np, pd, px
 
 
 if __name__ == "__main__":
